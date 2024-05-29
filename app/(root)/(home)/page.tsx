fa639bbc33ea9ff83c31a40760ca6dc89b9a0f80
saksham-chopra <sakshamc03@gmail.com>
2024-05-29 09:55:54 +0530

    first commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import MeetingTypeList from "@/components/MeetingTypeList";
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  // Update the current time every second
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Clear the interval on unmount to prevent memory leaks
+    return () => clearInterval(timer);
+  }, []);
+
+  const time = currentTime.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "Asia/Kolkata",
+  });
+  const date = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+    timeZone: "Asia/Kolkata",
+  }).format(currentTime);
+
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full f lex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+            Create Your own meeting Room
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
+
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
